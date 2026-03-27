b2331d8d59687ef08947c668ad08b9c625aa77a3
futuresoundz <271075570+futuresoundz@users.noreply.github.com>
2026-03-26 23:12:45 -0700

    Update NoiseSpace.java

--- NoiseSpace.java
+++ NoiseSpace.java
@@ -1,4 +1,5 @@
 import org.bukkit.*;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,45 +12,40 @@ public class NoiseSpace extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // I HOPE THIS WORKS LOL
-        Bukkit.getPluginManager().registerEvents(this, this);
-        System.out.println("!!! SPACE MOD LOADED !!! LETS GOO");
         
-        // This makes the oxygen work i think?
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-            @Override
-            public void run() {
-                for (Player p : Bukkit.getOnlinePlayers()) {
-                    // Check if they have a glass block on their head
-                    if (p.getInventory().getHelmet() != null) {
-                        if (p.getInventory().getHelmet().getType() == Material.GLASS) {
-                            // They are safe for now...
-                        } else {
-                            p.damage(2);
-                            p.sendMessage(ChatColor.RED + "CANT BREATHE!!! GET A HELMET!!!");
-                        }
-                    } else {
-                        // If helmet is null they die
-                        p.damage(2);
-                        p.sendMessage(ChatColor.RED + "CANT BREATHE!!! GET A HELMET!!!");
+        Bukkit.getPluginManager().registerEvents(this, this);
+        System.out.println("Space Mod Loaded");
+        
+
+        new BukkitRunnable() {
+            @Override 
+            public void run(){
+                for (Player p : Bukkit.getOnlinePlayers()){
+
+                    if (p.getInventory().getHelmet() != null && p.getInventory().getHelmet().getType() == Material.GLASS) {
+                        p.sendMessage(ChatColor.GREEN + "Sweet Helmet.");
+                    }
+                    else {
+                       p.damage(1);
+                        p.sendMessage(ChatColor.RED + "CAN'T BREATHE! PUT ON HELMET! SEND FOR HELP!!");
                     }
                 }
             }
-        }, 0L, 40L);
+        }.runTaskTimer(this, 0L, 40L);
+
     }
 
     @EventHandler
     public void move(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         
-        // Make them floaty like a spaceman
+        
         p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 100, 2));
         p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 100, 1));
         
-        // Cool dust effects
+        
         p.getWorld().spawnParticle(Particle.WHITE_ASH, p.getLocation(), 10);
         
-        // Random message so they know the mod is working
-        // p.sendMessage("You are moving in space!"); // TODO: delete this later its annoying
+        
     }
 }
